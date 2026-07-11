dae4e44a49729709e2e06297cc0731aeed2c7b3d
OtterFan <ottercorporationofficial@gmail.com>
2026-07-10 21:05:06 -0400

    fixed link

--- frontend/static/js/app.js
+++ frontend/static/js/app.js
@@ -33,7 +33,7 @@ const API =
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1"
         ? "http://127.0.0.1:8000"
-        : "election-tracker-production.up.railway.app";
+        : "https://election-tracker-production.up.railway.app";
 
 
 
